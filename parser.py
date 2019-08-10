15a32f15d6fcc1199246b330dbf8d68b67bda55f
Lewis King <lewis.king@fauna.com>
2019-08-09 19:20:29 -0500

    Created the final_data dictionary

--- parser.py
+++ parser.py
@@ -47,9 +47,62 @@ def parse_ssc_file(filename=None):
         else:
             parsed[k] = v
 
-    print(repeated)
+    #print(repeated)
+    #return parsed
 
-    return parsed
+    #final_data contains the JSON/dictionary that will be put into MongoDB
+    final_data = {
+        "song_name": None,
+        "song_artist": None,
+        "bpm": None,
+        "pack_name": None,   # read_meta.py will be updated and eventually fill this in at the db_insert level
+        "pack_link": None,
+        "difficulty": {
+            "Challenge": None,
+            "Hard": None,
+            "Medium": None,
+            "Easy": None,
+            "Beginner": None,
+            "Edit": None
+        }
+    }
+    """
+    Expected structure:
+    {
+        "song_name": "Dream a Dream",
+        "song_artist": "Captain Jack",
+        "bpm": "120",
+        "pack_name": "DDR MAX 2",
+        "pack_link": "drive.google.com/onemoretimeimbackwithanewrhyme",
+        "difficulty": {
+            "light": "3",
+            "standard": "5",
+            "heavy": "7"
+        }
+    }
+    """
+    final_data["song_name"] = parsed["title"]
+    final_data["song_artist"] = parsed["artist"]
+    #cast to int and round
+    final_data["bpm"] = repeated["displaybpm"][0]
+    """
+    ### ensuring BPM is correctly represented
+    bpm = repeated["displaybpm"][0]
+    for i in repeated["displaybpm"]:
+        if repeated["displaybpm"][i] != bpm:
+            final_data["bpm"] = 'various'
+    """
+    #there could be dupe difficulties. this assumes there are not.
+    num_diffs = len(repeated["difficulty"])
+    i = 0
+    while i < num_diffs:
+        # Find the current difficulty in the array
+        current_diff = repeated["difficulty"][i]
+        #Assign the respective meter value to that difficulty, in our final_data dictionary
+        final_data["difficulty"][current_diff]=repeated["meter"][i]
+        i+=1
+
+    return final_data
 
 
 parsed_ssc = parse_ssc_file('night.ssc')
