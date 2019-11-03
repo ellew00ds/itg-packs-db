ca0018a4f8ceb67b8b67a6ca6799919f5ac01331
Lewis King <lewis.king@fauna.com>
2019-11-02 19:20:40 -0500

    Adding output to songinfo.json

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from models import Pack, FaunaDBLoader
-# ddr = Pack.from_path("/Users/lewis/workspace/itg-packs-db/packs/Dance Dance Revolution 3rd Mix/")
-dimo = Pack.from_path("/Users/lewis/workspace/itg-packs-db/packs/dimo/")
+from pathlib import Path
+
+EXCLUDED_FILENAMES = {'.DS_Store'}
+packs_path = Path('packs/')
+
+with open('songinfo.json', 'w') as fp:
+	for pack_path in filter(lambda x: x.name not in EXCLUDED_FILENAMES, packs_path.iterdir()):
+		pack = Pack.from_path(pack_path)
+		for song in pack.songs:
+			fp.write(song.to_json())
+			fp.write('\n')
+
+"""
 loader = FaunaDBLoader()
 # Ultimately this will load these songs into FaunaDB
 loader.load(dimo.songs)
 
 for song in dimo.songs:
 	print(song.to_json())
+"""
